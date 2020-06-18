d700d2d9418ae2011f560022254ca99b5bf673fe
huvuftvbz <huvuftvbz@protonmail.com>
2020-06-18 15:37:24 +0200

    modified

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,18 +1,11 @@
 #!/bin/sh
-var=$(printf "i3-keys\ni3-start\ni3-theme\nkitty\nzsh\nvim" | rofi -dmenu -i -p "Dotfiles: " -theme themes/dmenu.rasi)
+var=$(printf "i3\nkitty\nzsh\nvim" | rofi -dmenu -i -p "Dotfiles: " -theme themes/dmenu.rasi)
 
 
 case $var in
-    'i3-keys') 
-	gvim ~/.config/i3/keybindings
-	;;
-
-    'i3-start') 
-	gvim ~/.config/i3/autostart
-	;;
 
     'i3-theme') 
-	gvim ~/.config/i3/theme
+	gvim ~/.config/i3/config
 	;;
 
     'kitty')
